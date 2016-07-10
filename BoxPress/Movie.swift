9d6048e47ec4a2238acd129c9552893abe9265a3
Yoshikuni Kato <yoshikuni.kato.g@gmail.com>
2016-07-10 23:16:05 +0900

    Reduce models

--- BoxPress/Movie.swift
+++ BoxPress/Movie.swift
@@ -31,28 +31,31 @@ extension Movie {
 
 extension Movie {
     static let movies = [
-        Movie(title: "Front-Back", frameImageFileNamePrefix: "frontback"),
-        Movie(title: "Face Panning", frameImageFileNamePrefix: "IMG_0457"),
-        Movie(title: "Right-Left", frameImageFileNamePrefix: "rightleft"),
-        Movie(title: "Rolling", frameImageFileNamePrefix: "rolling"),
-        Movie(title: "Up-Down", frameImageFileNamePrefix: "updown", frameOrder: .Descending),
-        Movie(title: "Hand Waving", frameImageFileNamePrefix: "wavehands"),
-        Movie(title: "iPhone Lock", frameImageFileNamePrefix: "lock"),
-        Movie(title: "iPhone open photo", frameImageFileNamePrefix: "openPhoto"),
-        Movie(title: "3d map", frameImageFileNamePrefix: "mapreal"),
-        Movie(title: "calendar", frameImageFileNamePrefix: "calendar"),
-        Movie(title: "camera", frameImageFileNamePrefix: "camera"),
-        Movie(title: "compass", frameImageFileNamePrefix: "compass"),
-        Movie(title: "flatmap", frameImageFileNamePrefix: "flatmap"),
-        Movie(title: "notificationCenter", frameImageFileNamePrefix: "notificationCenter"),
-        Movie(title: "phone", frameImageFileNamePrefix: "phone"),
+//        Movie(title: "Front-Back", frameImageFileNamePrefix: "frontback"),
+//        Movie(title: "Face Panning", frameImageFileNamePrefix: "IMG_0457"),
+//        Movie(title: "Right-Left", frameImageFileNamePrefix: "rightleft"),
+//        Movie(title: "Rolling", frameImageFileNamePrefix: "rolling"),
+//        Movie(title: "Up-Down", frameImageFileNamePrefix: "updown", frameOrder: .Descending),
+//        Movie(title: "Hand Waving", frameImageFileNamePrefix: "wavehands"),
+
+//        Movie(title: "iPhone open photo", frameImageFileNamePrefix: "openPhoto"),
+//        Movie(title: "3d map", frameImageFileNamePrefix: "mapreal"), // option 2
+
+//        Movie(title: "camera", frameImageFileNamePrefix: "camera"), // option 2
+
+//        Movie(title: "flatmap", frameImageFileNamePrefix: "flatmap"),
+//        Movie(title: "notificationCenter", frameImageFileNamePrefix: "notificationCenter"),
+//        Movie(title: "phone", frameImageFileNamePrefix: "phone"), // option 2
         Movie(title: "paper", frameImageFileNamePrefix: "paper"),
         Movie(title: "write on paper", frameImageFileNamePrefix: "paper_write"),
-        Movie(title: "paper_write_crash", frameImageFileNamePrefix: "paper_write_crash"),
-        Movie(title: "babystar", frameImageFileNamePrefix: "babystar"),
+//        Movie(title: "paper_write_crash", frameImageFileNamePrefix: "paper_write_crash"),
+//        Movie(title: "babystar", frameImageFileNamePrefix: "babystar"),
         Movie(title: "books", frameImageFileNamePrefix: "books"),
         Movie(title: "bottle_lay", frameImageFileNamePrefix: "bottle_lay"),
          Movie(title: "bottle_rolling", frameImageFileNamePrefix: "bottle_rolling"),
-         Movie(title: "water", frameImageFileNamePrefix: "water")
+         Movie(title: "water", frameImageFileNamePrefix: "water"),
+         Movie(title: "calendar", frameImageFileNamePrefix: "calendar"),
+         Movie(title: "compass", frameImageFileNamePrefix: "compass"),
+         Movie(title: "iPhone Lock", frameImageFileNamePrefix: "lock")
     ]
 }
